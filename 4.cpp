beb7ec5b1bd14072104f40d3ba0e15a65a6c626b
m-9 <m-9@yandex.ru>
2021-08-11 01:45:29 +0500

    Оставил комментарии в тексте каждой задачи.

--- 4.cpp
+++ 4.cpp
@@ -2,45 +2,86 @@
 #include <string>
 #include <cstdlib>
 #include <cctype>
+#include <limits>
+#include <ctime>
 
 using namespace std;
-//http://cppstudio.com/post/2668/
-int main()
-{
-    int n, m, sum=0;
-    cout<<"Enter n and m "<<endl;;
-    cin>>n;
-    cin>>m; cout<<" "<<endl;
-    int **p = new int *[n];
-    for (int i =  0; i < n; ++i)
-{
-    p[i] = new int [m];
+
+// http://cppstudio.com/post/2668/
+
+// всё, что делается единообразно и более одного раза - кандидат на вынос в функцию
+// Особенно приятно, когда уже есть готовая функция из 1.cpp
+int inputN(const char* txt){
+    int n;
+
+    cout << txt;
+    while (! (cin >> n)){
+        cout << "Your entry must be a valid numeric value." << endl; 
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << txt;
+    }
+
+    return n;
 }
 
-    for (int i=0; i<n; i++)
-    {
-        for (int j=0; j<m; j++)
-        {
+int main()
+{
+    // Даём значимым переменным говорящие имена, не жадничаем. Текст программы резко становится понятнее!
+    int rows, columns, sum=0;
+    
+    // при работе со случайными числами надо инициализовывать генератор, иначе случайности не будет
+    srand( time(0) );
 
-            p[i][j]=rand()%30-10;
-            cout.width(3);
-            cout<<p[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-    for (int j=0; j<m; j++)
+    rows = inputN("Enter the number of rows: ");
+    columns =  inputN("Enter the number of columns: ");
+    cout << " " << endl;
+
+    int **p = new int *[rows];
+    for (auto i =  0; i < rows; ++i)
     {
-        for (int i=0; i<n; i++)
-        {
-            if (p[i][j]>=0) {sum+=p[i][j];}
-            else {sum=0;break;}
-        }
-        if (sum>0) cout<<sum<<" ";
+        p[i] = new int [columns];
     }
-    for (int i; i<n; i++)
-    delete[] p[i];
+
+    for (auto i = 0; i < rows; i++)
+    {
+        for (auto j = 0; j < columns; j++)
+        {            
+            p[i][j] = rand() % 30 - 10;
+            cout.width(3);
+            cout << p[i][j] << " ";
+        }
+        cout << endl;
+    }
+    for (auto j = 0; j < columns; j++)
+    {
+        for (auto i=0; i < rows; i++)
+        {
+            if (p[i][j] >= 0){
+                sum += p[i][j];
+            }
+            else {
+                sum=0;
+                break;
+            }
+        }
+        // Лучше подписывать, в каком столбце у нас вычислялась сумма
+        // И помним, что программисты считают с 0, а люди - с 1. Программа обычно пишется для людей.
+        if (sum>0){ 
+            cout << "Column " << j+1 << " sum="<< sum << endl;
+        }
+        // и ошибка. В задаче сказано найти сумму каждого столбца по отдельности. Нужно обнулять сумму, либо создавать для суммирования локальную переменную
+        sum = 0;
+    }
+    
+    // Патриархи, Кенниган и Ритчи, даровали нам операторные скобки, нужно их использовать.
+    // Иначе есть шанс потратить лучшие дни жизни в поисках ошибки
+    for (auto i = 0; i < rows; i++) {
+        delete[] p[i];
+    }
     delete[] p;
 
     return 0;
 }
 
+// А в целом к этой задаче больше замечаний нет, кроме того, что исправил.
